c1ba9e56538ce0616e817ffb1cecd63699f3d306
Johnny <johnny230301@tyr-tech.com>
2023-03-22 15:50:37 +0800

    added iview form

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,35 +1,68 @@
 <template>
-  <Collapse v-model="value1">
-    <Panel name="1">
-      史蒂夫·乔布斯
-      <p slot="content">
-        史蒂夫·乔布斯（Steve
-        Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。
-      </p>
-    </Panel>
-    <Panel name="2">
-      斯蒂夫·盖瑞·沃兹尼亚克
-      <p slot="content">
-        斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary
-        Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC
-        Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。
-      </p>
-    </Panel>
-    <Panel name="3">
-      乔纳森·伊夫
-      <p slot="content">
-        乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。
-      </p>
-    </Panel>
-  </Collapse>
+  <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+    <FormItem prop="user">
+      <Input type="text" v-model="formInline.user" placeholder="Username">
+        <Icon type="ios-person-outline" slot="prepend"></Icon>
+      </Input>
+    </FormItem>
+    <FormItem prop="password">
+      <Input
+        type="password"
+        v-model="formInline.password"
+        placeholder="Password"
+      >
+        <Icon type="ios-lock-outline" slot="prepend"></Icon>
+      </Input>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+    </FormItem>
+  </Form>
 </template>
 
-<style lang="scss" scoped>
-#SecondVuex {
-  h1 {
-    font-size: 50px;
-    color: lightgreen;
-    text-align: center;
-  }
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+        user: "",
+        password: "",
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "Please fill in the user name",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please fill in the password.",
+            trigger: "blur",
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "The password length cannot be less than 6 bits",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleSubmit(name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success("Success!");
+        } else {
+          this.$Message.error("Fail!");
+        }
+      });
+    },
+  },
+};
+</script>
+
