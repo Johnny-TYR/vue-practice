86ce5f2b51733cb81a0e78a6197850d9ce04bdcb
Johnny <johnny230301@tyr-tech.com>
2023-03-24 11:57:28 +0800

    moved product lists into a folder

--- src/views/VuexView.vue
+++ src/views/VuexView.vue
@@ -10,10 +10,10 @@
 <script>
 export default {
   components: {
-    ProductListOne: () => import("@/components/ProductListOne"),
-    ProductListTwo: () => import("@/components/ProductListTwo.vue"),
-    ProductListThree: () => import("@/components/ProductListThree.vue"),
-    ProductListFour: () => import("@/components/ProductListFour.vue"),
+    ProductListOne: () => import("@/components/ProductLists/ProductListOne"),
+    ProductListTwo: () => import("@/components/ProductLists/ProductListTwo.vue"),
+    ProductListThree: () => import("@/components/ProductLists/ProductListThree.vue"),
+    ProductListFour: () => import("@/components/ProductLists/ProductListFour.vue"),
   },
 };
 </script>
