21d1200b41ca2cf8a93925d3154137af5e78a50f
Johnny <johnny230301@tyr-tech.com>
2023-03-25 14:51:38 +0800

    finished basic datepicker

--- src/components/DatePickers/DatePicker1.vue
+++ src/components/DatePickers/DatePicker1.vue
@@ -1,4 +1,32 @@
 <template lang="pug">
-#DatePicker
- DatePicker
+#DatePicker1
+  h2.desc {{ "type=date/daterange，可以用 placement 決定彈窗的出現位置" }}
+  .container
+    DatePicker
+      Row
+        Col(span=12)
+          DatePicker(type="date", placeholder="date")
+        Col(span=12)
+          DatePicker(
+            type="daterange",
+            placeholder="daterange",
+            placement="bottom-end"
+          )
+  h2.desc {{ "adfa" }}
+  .container
+    DatePicker
 </template>
+
+<style lang="scss" scoped>
+#DatePicker1 {
+  .desc {
+    margin: 10px;
+  }
+  .container {
+    border: 1px solid black;
+    width: 1000px;
+    padding: 50px;
+    margin: 10px;
+  }
+}
+</style>
