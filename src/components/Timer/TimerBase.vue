c5341df23effaf8da44c8d88462413a0b8234735
Johnny <johnny230301@tyr-tech.com>
2023-03-27 11:24:24 +0800

    basic second interval

--- src/components/Timer/TimerBase.vue
+++ src/components/Timer/TimerBase.vue
@@ -1,4 +1,48 @@
 <template lang="pug">
 #TimerBase
-  h2 adkfadkfad
+  .TimerBase
+    h1 {{ time }}
+    .btnContainer
+      Button(@click="start") {{ "start" }}
+      Button(@click="stop") {{ "stop" }}
+      Button(@click="reset") {{ "reset" }}
 </template>
+
+<script>
+export default {
+  name: "TimerBase",
+  data() {
+    return {
+      time: 0,
+      runClock: null,
+    };
+  },
+
+  methods: {
+    start() {
+      this.$Message.success("Start");
+      this.runClock = setInterval(() => {
+        this.time++;
+      }, 1000);
+    },
+    stop() {
+      this.$Message.error("stop");
+      clearInterval(this.runClock)
+    },
+    reset() {
+      this.$Message.info("reset");
+      this.time = 0
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#TimerBase {
+  .TimerBase {
+    outline: auto;
+  }
+}
+#TimerBase {
+}
+</style>
--- src/views/iView.vue
+++ src/views/iView.vue
@@ -1,20 +1,80 @@
 <template>
   <div>
-    <Button type="primary" @click="open(false)">Open notice</Button>
-    <Button @click="open(true)">Open notice(only title)</Button>
+    <div id="clock">{{ time }}</div>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+    <button @click="reset">Reset</button>
   </div>
 </template>
+
 <script>
 export default {
+  data() {
+    return {
+      time: "00:00:00.000",
+      timeBegan: null,
+      timeStopped: null,
+      stoppedDuration: 0,
+      started: null,
+      running: false,
+    };
+  },
   methods: {
-    open(nodesc) {
-      this.$Notice.open({
-        title: "Notification title",
-        desc: nodesc
-          ? ""
-          : "Here is the notification description. Here is the notification description. ",
-      });
+    start() {
+      if (this.running) return;
+
+      if (this.timeBegan === null) {
+        this.reset();
+        this.timeBegan = new Date();
+      }
+
+      if (this.timeStopped !== null) {
+        this.stoppedDuration += new Date() - this.timeStopped;
+      }
+
+      this.started = setInterval(this.clockRunning, 10);
+      this.running = true;
+    },
+    stop() {
+      this.running = false;
+      this.timeStopped = new Date();
+      clearInterval(this.started);
+    },
+    reset() {
+      this.running = false;
+      clearInterval(this.started);
+      this.stoppedDuration = 0;
+      this.timeBegan = null;
+      this.timeStopped = null;
+      this.time = "00:00:00.000";
+    },
+    clockRunning() {
+      const currentTime = new Date();
+      const timeElapsed = new Date(
+        currentTime - this.timeBegan - this.stoppedDuration
+      );
+      const hour = timeElapsed.getUTCHours();
+      const min = timeElapsed.getUTCMinutes();
+      const sec = timeElapsed.getUTCSeconds();
+      const ms = timeElapsed.getUTCMilliseconds();
+
+      this.time =
+        zeroPrefix(hour, 2) +
+        ":" +
+        zeroPrefix(min, 2) +
+        ":" +
+        zeroPrefix(sec, 2) +
+        "." +
+        zeroPrefix(ms, 3);
     },
   },
 };
+
+function zeroPrefix(num, digit) {
+  let zero = "";
+  for (let i = 0; i < digit; i++) {
+    zero += "0";
+  }
+  return (zero + num).slice(-digit);
+}
 </script>
