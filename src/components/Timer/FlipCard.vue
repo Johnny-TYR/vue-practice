b2c6cd04e96fe41104f43ecae994f91b96d1d025
Johnny <johnny230301@tyr-tech.com>
2023-03-28 10:22:00 +0800

    full flip completed

--- src/components/Timer/FlipCard.vue
+++ src/components/Timer/FlipCard.vue
@@ -15,19 +15,20 @@
   // 佈局 CSS
   .flip-card {
     color: blue;
-    font-size: 25rem;
-    font-weight: bolder;
-    font-family: Arial, Helvetica, sans-serif;
     display: inline-flex;
     flex-direction: column;
     border-radius: 0.1em;
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
     position: relative;
-    // divide into two halves
+    // decoration only
+    font-size: 25rem;
+    font-weight: bolder;
+    font-family: Arial, Helvetica, sans-serif;
+    // divide into two halves ======================================================================
     .top {
       background-color: #f7f7f7;
       @extend .top-border;
-      border-bottom: 3px solid rgba(0, 0, 0, 0.1);
+      // border-bottom: 3px solid rgba(0, 0, 0, 0.1);
     }
     .bottom {
       background-color: white;
@@ -42,9 +43,8 @@
       line-height: 1;
       overflow: hidden;
     }
-    // make top half one animation, bottom half one animation using before/after
   }
-  // 動畫個別的 flap，一個上到中，一個中到下
+  // 動畫個別的 flap，一個上到中，一個中到下 ============================================================
   .flip::after,
   .flip::before {
     height: 0.75em;
@@ -52,29 +52,44 @@
     line-height: 1;
     overflow: hidden;
   }
+  // top animation
   .flip::before {
     content: "5";
     position: absolute;
-    color: black;
-    background-color: red;
     width: 100%;
+    background-color: #f7f7f7;
     @extend .top-border;
     animation: flip-top 1000ms ease-in;
     transform-origin: bottom;
+    border-bottom: 3px solid rgba(0, 0, 0, 0.1);
   }
-
   @keyframes flip-top {
     100% {
       transform: rotateX(90deg);
     }
   }
+  // bottom animation
   .flip::after {
+    content: "5";
+    position: absolute;
+    bottom: 0;
+    color: blue;
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+    @extend .bottom-border;
+    animation: flip-bottom 1000ms ease-out;
+    transform-origin: top;
+    transform: rotateX(90deg);
+    animation-delay: 1000ms;
+  }
+  @keyframes flip-bottom {
+    100% {
+      transform: rotateX(0deg);
+    }
   }
 }
-
-#FlipCard {
-}
-
+// ==========================================================================================
 // 角的弧度
 .top-border {
   border-top-right-radius: 0.1em;
