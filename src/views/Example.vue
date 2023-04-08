afceed60459bbdc0283e5478f3e19b9027a53fd0
Johnny Chang <johnny22375@gmail.com>
2023-04-08 17:49:35 +0800

    made transition with named transitions

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
 #Transitions
-  div(style="height: 170px")
-    transition
-      .block(v-show="isShow") {{ "Hello Vue" }}
+  div(style="height: 350px")
+    transition(name="custom-fade")
+      .block(v-show="isShow") {{ "Custom Fade" }}
+    div(style="overflow: hidden; width: 150px")
+      transition(name="custom-slide")
+        .block(v-show="isShow") {{ "Custom Slide" }}
   Button(@click="isShow = !isShow") {{ "Toggle" }}
 </template>
 
@@ -18,36 +21,50 @@ export default {
 
 <style lang="scss" scoped>
 #Transitions {
+  padding: 20px;
   .block {
     width: 150px;
     height: 150px;
-    background-color: yellow;
+    background-color: lightgreen;
+    margin-top: 10px;
   }
 }
 // ========== Enter ==========
 // 進場「之前」的樣式
-.v-enter {
+.custom-fade-enter {
   opacity: 0;
 }
 // 進場「過程」的樣式
-.v-enter-active {
+.custom-fade-enter-active {
   transition: opacity 1s;
 }
 // 進場「結束」的樣式
-.v-enter-to {
+.custom-fade-enter-to {
   opacity: 1;
 }
 // ========== Leave ==========
 // 退場「之前」的樣式
-.v-leave {
+.custom-fade-leave {
   opacity: 1;
 }
 // 退場「過程」的樣式
-.v-leave-active {
+.custom-fade-leave-active {
   transition: opacity 1s;
 }
 // 退場「結束」的樣式
-.v-leave-to {
+.custom-fade-leave-to {
   opacity: 0;
+}
+
+// Custom Slide animations ==========
+.custom-slide-enter {
+  transform: translateX(-100%);
+}
+.custom-slide-enter-active,
+.custom-slide-leave-active {
+  transition:all ease-in-out 1s;
+}
+.custom-slide-leave-to {
+  transform: translateX(100%);
 }
 </style>
