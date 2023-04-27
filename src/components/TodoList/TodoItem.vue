337d89b6f0c82a6d5894359bba4e6d6a9522f5f8
Johnny <johnny230301@tyr-tech.com>
2023-04-27 09:54:45 +0800

    added todoItem file

---
+++ src/components/TodoList/TodoItem.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+//- 請填寫功能描述👈
+#TodoItem
+  p TodoItem
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  data () {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// 排版
+#TodoItem {
+}
+// 元件
+#TodoItem {
+}
+</style>
--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -2,19 +2,18 @@
 #TodoList
   .todo-container
     h1 {{ "Todo List" }}
+    TodoItem
 </template>
 
 <script>
 export default {
   name: "TodoList",
+  components: {
+    TodoItem: () => import("@/components/TodoList/TodoItem.vue")
+  },
   data() {
     return {
-      test: "qweadfkjahsldkfjhae"
-    }
-  },
-  computed: {
-    todoList() {
-      this.$store.state.todoList
+
     }
   }
 }
