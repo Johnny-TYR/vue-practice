0043202f36afba61a2c3dd2016975e482f983695
Johnny <johnny230301@tyr-tech.com>
2023-05-09 15:24:09 +0800

    added notes

--- src/components/Auth/LineLogin.vue
+++ src/components/Auth/LineLogin.vue
@@ -13,13 +13,16 @@ export default {
   },
   methods: {
     LineLogin() {
+      // 設定會用到的參數
       let urlQuery = {
-        response_type: "code",
-        client_id: "1661076147",
-        redirect_uri: "http://localhost:8080/timer",
-        state: "login",
-        scope: "profile%20openid%20email"
+        response_type: "code",  // 會回傳參數名code的授權碼
+        client_id: "1661076147",  // line dev 建立的 channel ID
+        redirect_uri: "http://localhost:8080/timer",  // line dev 設定登入後要引導到哪一頁
+        state: "login", // 自訂驗證碼，隨便填
+        scope: "profile%20openid%20email" // 要取得user的哪些資料，去docs看，如果要 email 還要先特別設定
       }
+
+      // 要回傳的 URL
       let URL = "https://access.line.me/oauth2/v2.1/authorize?";
       URL += `response_type=${urlQuery.response_type}`;
       URL += `&client_id=${urlQuery.client_id}`;
@@ -27,6 +30,8 @@ export default {
       URL += `&state=${urlQuery.state}`;
       URL += `&scope=${urlQuery.scope}`;
       console.log(URL)
+      
+      // 重新導向 URL
       window.location.href = URL
     }
   }
@@ -41,6 +46,7 @@ export default {
 #LineLogin {
   .line-login-btn {
     width: 150px;
+
     &:active {
       transform: scale(0.99);
     }
